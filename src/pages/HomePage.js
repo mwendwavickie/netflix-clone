f2e7817474ecba70ee8136830cb2708a3233008f
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-06-16 14:05:30 +0300

    Fetch and display trending, top-rated, and action movies in HomePage component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,52 +1,52 @@
-import React from "react";
-import { Container, Typography, Grid, Card, CardMedia, CardContent } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from 'react';
-//import movieRow from "../components/movieRow";
+import { Container } from "@mui/material";
+import MovieRow from "../components/MovieRow";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 const TRENDING_URL = `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`;
+const TOPRATED_URL = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`;
+const ACTION_URL = `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=28&language=en-US`;
 
 const HomePage = () => {
-    const [trendingMovies, setTrendingMovies] = useState([]);
-    //const [topRated, setTopRated] = useState([]);
-    //const [actionMovies, setactionMovies] = useState([]);
+    const [trending, setTrending] = useState([]);
+    const [topRated, setTopRated] = useState([]);
+    const [actionMovies, setActionMovies] = useState([]);
 
     useEffect(() => {
-        axios.get(TRENDING_URL)
-            .then((res) => setTrendingMovies(res.data.results))
-            .catch((error) => console.error("Error fetching trending movies:", error));
+        const fetchData = async () => {
+            try {
+                // Fetch data from TMDB API
+                const trendingRes = await axios.get(TRENDING_URL);
+                const topRatedRes = await axios.get(TOPRATED_URL);
+                const actionRes = await axios.get(ACTION_URL);
+                // Set the state with the fetched data
+
+                setTrending(trendingRes.data.results);
+                setTopRated(topRatedRes.data.results);
+                setActionMovies(actionRes.data.results);
+            } catch (error) {
+                console.error("Error fetching data from TMDB API:", error);
+            }
+        };
+        fetchData();
+        
     }, []);
 
     return (
-        <Container sx={{ backgroundColor: '#111', minHeight: '100vh', padding: '20px' }}maxWidth="false">
-            <Typography variant="h4" component="h1" gutterBottom sx={{ color: '#fff', textAlign: 'center' }}>
-                Trending this week
-            </Typography>
+        <Container 
+        sx={{ 
+            backgroundColor: '#111', 
+            minHeight: '100vh', 
+            padding: '20px' 
+        }}
+        maxWidth="false"
+        >
+            <MovieRow title="Trending Now" movies={trending} />
+            <MovieRow title="Top Rated" movies={topRated} />
+            <MovieRow title="Action Movies" movies={actionMovies} />
 
-            <Grid container spacing={4}>
-                {trendingMovies.map((movie) => (
-                    <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
-                        <Card sx={{ backgroundColor: '#222', color: '#fff' }}>
-                            <CardMedia
-                                component="img"
-                                height="300"
-                                image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                alt={movie.title}
-                            />
-                            <CardContent>
-                                <Typography variant="h6" component="div">
-                                    {movie.title}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {movie.release_date}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
         </Container>
     )
 }
