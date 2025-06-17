25a28c6ea1a6c972fb43fb93fede4bafe3283c26
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-06-17 16:42:55 +0300

    Add routing for Login, Signup, and SearchResults pages; include Navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import MovieDetailPage from './pages/MovieDetailPage';
+import SearchResultsPage from './pages/SearchResultsPage';
+import LoginPage from './pages/LoginPage';
+
+
 
 function App() {
   return (
     <Router>
+      <Navbar />
      <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/signup' element={<div> signup page </div>} />
       <Route path="/movie/:id" element={<MovieDetailPage />} />
+      <Route path="/search" element={<SearchResultsPage />} />
      </Routes>
    </Router>
   );
