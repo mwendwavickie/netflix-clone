2ad088fc2335050195e8a5f5e003ad5fc52ff418
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-06-17 12:28:36 +0300

    Add route for MovieDetailPage to display individual movie information

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MovieDetailPage from './pages/MovieDetailPage';
 
 function App() {
   return (
     <Router>
      <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MovieDetailPage />} />
      </Routes>
    </Router>
   );
