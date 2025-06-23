93438432271ab7990ad409ab232809d571061888
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-06-23 22:20:21 +0300

    Refactor Firebase configuration to use environment variables for sensitive data

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "netflix-clone.firebaseapp.com",
-    projectId: "netflix-clone-29d49",
-    appId: "YOUR_APP_ID",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
